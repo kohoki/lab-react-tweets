013cae0916f4a34aeb47eba78618bbc12c1f66f7
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-14 17:03:07 +0100

    it7 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,7 @@
 import ProfileImage from "./ProfileImage";
 import User from "./User";
 import Timestamp from "./Timestamp";
+import Message from "./Message";
 
 function Tweet({tweet}) {
 
@@ -16,10 +17,7 @@ function Tweet({tweet}) {
           <User userData={tweet.user}/>
           <Timestamp time={tweet.timestamp}/>
         </div>
-
-        <p className="message">
-          
-        </p>
+          <Message message={tweet.message}/>
 
         <div className="actions">
           {/* Font Awesome icons */}
